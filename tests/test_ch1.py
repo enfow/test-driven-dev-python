32f9c1aa3e041dfa9d0b86dc957916a83c5eb2fc
enfow <wgm0601@gmail.com>
2020-11-21 15:47:24 +0900

    move ch1.md to

--- tests/test_ch1.py
+++ tests/test_ch1.py
@@ -2,10 +2,11 @@
 
 from ch1.dollar import Dollar
 
-
 class TestDollar:
+    """Test Dollar"""
     def test_multiplication(self):
         """test code"""
         five = Dollar(5)
         five.times(2)
         assert 10 == five.amount
+
